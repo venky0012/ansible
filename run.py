de8a1eec633c54c828eca37b42ca699098ba49ce
Venky0012 <bvenkatreddy0012@gmail.com>
2022-07-24 03:04:41 +0530

    testing

--- run.py
+++ run.py
@@ -4,9 +4,12 @@ import subprocess
 file = "playbooks/" + str(sys.argv[1])
 print(file)
 run = subprocess.run(['ansible-playbook', file], capture_output = True)
-
+# output to text file
 file1 = (run.stdout).decode('utf-8')
 print(file1)
 ct = datetime.datetime.now()
 with open('outputs.txt', 'a') as f:
     f.write("\n"+"Time: " + str(ct)+"\n"+"File: "+ str(file)+ "\n"+ str(file1)+ "\n\n"+".............................................................................."+"\n")
+
+
+
